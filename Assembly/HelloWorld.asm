3a4b440f20ec28e76447ff828fc3989a5cb372a5
Yong Ju Kwak <amese1225@gmail.com>
2021-02-18 09:45:28 +0900

    변수와 레지스터

--- Assembly/HelloWorld.asm
+++ Assembly/HelloWorld.asm
@@ -18,10 +18,32 @@ CMAIN:
     mov cl, 0xff
     
     mov al, 0x00
-    mov rax, rdx
     
+    ; 메모리 <-> 레지스터
+    ; mov rax, a ; 주소값 복사
+    ; mov rax, [a] ; a라는 변수 안에 있는 값 복사
+    
+    mov al, [a]    
+    
+    mov [a], byte 0x55
+    mov [a], word 0x6666
+    mov [a], cl
     
     xor rax, rax
     ret
     
-;section .data
+    ; 초기화 된 데이터
+    ; [변수이름] [크기] [초기값]
+    ; [크기] db(1) dw(2) dd(4) dq(8)
+    
+section .data
+    a db 0x11
+    b dw 0x2222
+    c dd 0x33333333
+    d dq 0x4444444444444444
+    
+    ; 초기화 되지 않은 데이터
+    ; [변수이름] [크기] [개수]
+    ; [크기] resb(1) resw(2) resd(4) resq(8)
+section .bss
+    e resb 10
