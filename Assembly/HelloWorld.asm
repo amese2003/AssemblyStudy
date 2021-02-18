3f887af64b0a2d2886c7ceccb44e41e29fc4f1e9
Yong Ju Kwak <amese1225@gmail.com>
2021-02-18 10:06:39 +0900

    문자와 라디안

--- Assembly/HelloWorld.asm
+++ Assembly/HelloWorld.asm
@@ -5,29 +5,9 @@ global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
     
-    ; 8 bit = 1 byte
-    ; 16 bit = 2 bytes = 1 word
-    ; 32 bit = 4 bytes = 2 word = 1 dword (double-word)
-    ; 64 bit = 8 bytes = 4 word = 1 qword (quad-word)
+    PRINT_STRING msg  
     
-    ; mov reg1, cst
-    ; mov reg1, reg2
     
-    mov eax, 0x1234
-    mov rbx, 0x12345678
-    mov cl, 0xff
-    
-    mov al, 0x00
-    
-    ; 메모리 <-> 레지스터
-    ; mov rax, a ; 주소값 복사
-    ; mov rax, [a] ; a라는 변수 안에 있는 값 복사
-    
-    mov al, [a]    
-    
-    mov [a], byte 0x55
-    mov [a], word 0x6666
-    mov [a], cl
     
     xor rax, rax
     ret
@@ -37,10 +17,14 @@ CMAIN:
     ; [크기] db(1) dw(2) dd(4) dq(8)
     
 section .data
-    a db 0x11
-    b dw 0x2222
-    c dd 0x33333333
-    d dq 0x4444444444444444
+    msg db 'Hello World', 0x00
+    
+    a db 0x48,0x65,0x6c,0x6c,0x6f,0x20,0x57,0x6f,0x72,0x6c,0x64,0x0
+    
+    b dd 0x12345678
+     ; 장단점?
+     ; - 빅 엔디언 : 숫자 비교에 유리
+     ; - 리틀 엔디언 : 캐스팅에 유리 rax, eax, ax, ah, al
     
     ; 초기화 되지 않은 데이터
     ; [변수이름] [크기] [개수]
