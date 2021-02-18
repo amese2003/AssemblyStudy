41d356eea4b911642cf143ac18d5c63e4e153c18
Yong Ju Kwak <amese1225@gmail.com>
2021-02-18 21:01:36 +0900

    반복문

--- Assembly/HelloWorld.asm
+++ Assembly/HelloWorld.asm
@@ -5,50 +5,62 @@ global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
     
-    ; CMP dst, src (dst 기준)
-    ; 비교를 한 결과물은 Flag Register 저장
+    ; 반복문 (while, for)
     
-    ; JMP [label] 시리즈
-    ; JMP : 무조건 JUMP
-    ; JE : JumpEquals 같으면 jump
-    ; JNE : JumpNotEquals 다르면 jump
-    ; JG : JumpGreater 크면 jump
-    ; JGE : JumpGreaterEquals 크거나 같으면 jump
-    ; JL
-    ; JLE
+    ; ex) Hello world를 10번 출력해야하면?
+    
+    ; mov ecx, 10 
     
     
-    mov ax, 100
-    mov bl, 2    
-    div bl    
-    PRINT_DEC 1, ax    
-    CMP ah, 0
-    
-    JE LABEL_DUO
-    
-    mov rcx, 0
-    JMP LABEL_PASS
+;LABEL_LOOP:
+    ;PRINT_STRING msg
+    ;NEWLINE
+    ;dec ecx
+    ;cmp ecx, 0    
+    ;jne LABEL_LOOP
     
     
-LABEL_DUO:
-    mov rcx, 1
-LABEL_PASS:
+    ; 연습 문제) 1에서 100까지 합을 구할려면?
+    xor ebx, ebx
+    xor ecx, ecx
+    
+LABEL_LOOP:
+    add ebx, ecx
+    add ecx, 1
+    
+    cmp ecx, 101
+    
+    jne LABEL_LOOP
+    
+    PRINT_DEC 1, ebx
     NEWLINE
-    PRINT_DEC 1, rcx
     
+    ; loop [라벨]
+    ; - ecx에 반복 횟수
+    ; - loop 할때마다 ecx 1 감소 0이면 빠져나감. 아니면 라벨로 이동
     
+    mov ecx, 100
+    xor ebx, ebx
+LABEL_LOOP_SUM:
+    add ebx, ecx
+    loop LABEL_LOOP_SUM
+    
+    PRINT_DEC 1, ebx
+    NEWLINE
     
     
     
     
     xor rax, rax
     ret
+   
     
     ; 초기화 된 데이터
     ; [변수이름] [크기] [초기값]
     ; [크기] db(1) dw(2) dd(4) dq(8)
     
-;section .data
+section .data
+    msg db 'Hello World', 0x00
     
     ; 초기화 되지 않은 데이터
     ; [변수이름] [크기] [개수]
