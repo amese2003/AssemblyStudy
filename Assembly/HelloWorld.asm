3bc0fdc0ffafc6a7a470e31b28a390c6eaea75e7
Yong Ju Kwak <amese1225@gmail.com>
2021-02-18 23:24:10 +0900

    스택 메모리

--- Assembly/HelloWorld.asm
+++ Assembly/HelloWorld.asm
@@ -5,52 +5,40 @@ global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging    
     
-    ; 함수 (프로시저 procedure 서브루틴 subroutine)
-    ; call PRINT_MSG
+    ; 스택 메모리, 스택 프레임
     
+    ; 레지스터는 다양한 용도로 사용
+    ; - a b c d 범용 레지스터
+    ; - 포인터 레지스터 (포인터 = 위치를 가르키는 무언가)
+    ; -- ip (Insturction Pointer) : 다음 수행 명령어
+    ; -- sp (Stack Pointer) : 현재 스택 top 위치 (일종의 Cursor)
+    ; -- bp (Base Pointer) : 스택 상대주소 계산용
     
-    
-    mov eax, 10
-    mov ebx, 15
-    
+    push 1
+    push 2
     call MAX
-    PRINT_DEC 4, ecx
+    PRINT_DEC 8, rax
     NEWLINE
+    
+    add rsp, 16
     
     xor rax, rax
     ret
     
-PRINT_MSG:
-    PRINT_STRING msg
-    NEWLINE
-    ret
-    
-    ; ex) 두 값중 더 큰값을 반환하는 max
-    ; 근데 두 값을 어떻게 넘겨 받지? 반환은?
-    ; eax와 ebx 입력값을 ecx에 반환
-    
 MAX:
-    cmp eax, ebx
+    push rbp
+    mov rbp, rsp
+    
+    mov rax, [rbp + 16]
+    mov rbx, [rbp + 24]
+    cmp rax, rbx    
     jg L1
-    mov ecx, ebx,
-    jmp L2
-    
-    
+    mov rax, rbx
 L1:
-    mov ecx, eax
-L2:
+    pop rbp
     ret
     
     
-    ; 근데 인제가 10개라면?
-    ; eax, ebx에 중요한 값이 있으면?
-    ; [!] .data .bss 사용하면? 
-    ; 인자를 도대체 몇개를 할댕해야 할까?리
-    
-    ; 다른 메모리 구조가 필요하다
-    ; - 꿈이 유효한 동안에는 그 꿈을 유지시켜야 함 (유효범위 개념이 있다)
-    ; - 꿈이 끝나면 그 꿈을 부셔버려도 됨 (정리의 개념이 있다)
-    ; - 꿈에서도 또 꿈을 꿀 수 있다는 것을 고려 (유동적으로 유효 범위가 확장 가능해야함)
     
     ; [!] 스택(stack)이라는 메모리 영역을 사용
     ; 함수가 사용하는 일종의 메모장
@@ -64,9 +52,6 @@ L2:
     
 section .data
     msg db 'Hello World', 0x00
-    dd a 0
-    dd b 0
-    dd c 0
     
     ; 초기화 되지 않은 데이터
     ; [변수이름] [크기] [개수]
